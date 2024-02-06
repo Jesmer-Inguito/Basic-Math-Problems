4abb7c54c6da3c15e727f86225eb3d1d49840a17
unknown <you@example.com>
2024-02-06 21:38:20 +0800

    now working but not yet done

--- math4kids.py
+++ math4kids.py
@@ -6,17 +6,17 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # Function for the answer ; var1 is the solving Entry (solving = Entry(app))
 def answer(var1):
     if var1.get() == str(resultPLUS()):
-        correct = tk.Label(app, text="Correct!", fg="green", font=("Courier", 14))
-        correct.pack(padx=15, pady=15)
+        correct = Label(app, text="Correct!", fg="green", font=("Courier", 16))
+        correct.place(relx=0.3, rely=0.2)
     else:
-        wrong = tk.Label(app, text="Wrong!", fg="red", font=("Courier", 14))
-        wrong.pack(padx=15, pady=15)
+        wrong = Label(app, text="Wrong!", fg="red", font=("Courier", 16))
+        wrong.place(relx=0.3, rely=0.2)
 # Function to try again
 def try_again():
     try_again.num1update = random.choice(num)
     try_again.num2update = random.choice(num)
-    question = tk.Label(app, text=f"{try_again.num1update}+{try_again.num2update}", font=("Courier", 14))
-    question.pack(padx=10, pady=10)
+    question = Label(app, text=f"{try_again.num1update}+{try_again.num2update}", font=("Courier", 14))
+    question.place(relx=0.16, rely=0.14, relwidth=0.7, relheight=0.23)
 # Another function to try again
 def resultPLUS():
     try_again
@@ -24,29 +24,25 @@ def resultPLUS():
     return try_again.num1update + try_again.num2update
 
 # User Interface
-app = tk.Tk()
+app = Tk()
 # Title
 app.title("Math4Kids")
 
 # Window
-app.geometry("400x400")
-app.resizable(True, True)
-
-# Label
-label = tk.Label(app, text="MATH4KIDS", font=("Times New Roman", 18))
-label.pack(padx=20, pady=20)
+app.geometry("250x300")
+app.resizable(False, False)
 
 # Buttons and Textbox
-start = tk.Button(app, text="Press to Start", font=("Arial", 16), command=try_again)
-start.pack(padx=10, pady=10)
+start = Button(app, text="Start", command=try_again)
+start.place(relx=0.45, rely=0.2)
 
-solving = tk.Text(app, height=2, font=("Arial", 16))
-solving.pack(padx=5, pady=5)
+solving = Entry(app)
+solving.place(relx=0.35, rely=0.4, relwidth=0.34, relheight=0.23)
 
-submit = tk.Button(app, text="Submit", font=("Arial", 16), command=lambda: answer(solving))
-submit.pack(padx=10, pady=10)
+submit = Button(app, text="Submit", command=lambda: answer(solving))
+submit.place(relx=0.35, rely=0.64, relwidth=0.34, relheight=0.23)
 
-try_again = tk.Button(app, text="Press to Try Again", font=("Arial", 16), command=try_again)
-try_again.pack(padx=10, pady=10)
+try_again = Button(app, text="Try Again", command=try_again)
+try_again.place(relx=0.39, rely=0.9)
 
 app.mainloop()

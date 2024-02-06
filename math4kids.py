63774e7a3f5970770cc095ea4f6d6b3d37f44f71
unknown <you@example.com>
2024-02-06 21:07:14 +0800

    created a number list and created a function for the answer

--- math4kids.py
+++ math4kids.py
@@ -1,9 +1,19 @@
 import random
 import tkinter as tk
 
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Main Function
+# Function for the answer ; var1 is the solving Entry (solving = Entry(app))
+def answer(var1):
+    if var1.get() == str():
+        correct = tk.Label(app, text="Correct!", fg="green", font=("Courier", 14))
+        correct.pack(padx=15, pady=15)
+    else:
+        wrong = tk.Label(app, text="Wrong!", fg="red", font=("Courier", 14))
+answer()
+
 # User Interface
 app = tk.Tk()
-
 # Title
 app.title("Math4Kids")
 
@@ -16,8 +26,8 @@ label = tk.Label(app, text="MATH4KIDS", font=("Time New Roman", 18))
 label.pack(padx=20, pady=20)
 
 # Buttons and Textbox
-start = tk.Button(app, text="Press to Start", font=("Arial", 16))
-start.pack(padx=10, pady=10)
+solving = tk.Button(app, text="Press to Start", font=("Arial", 16))
+solving.pack(padx=10, pady=10)
 
 text = tk.Text(app, height=2, font=("Arial", 16))
 text.pack(padx=5, pady=5)
